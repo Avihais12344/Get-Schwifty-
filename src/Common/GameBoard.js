784d8ff3f33b796422e7717f5f9c8be9eeafa758
Avihai Shalom <avihais12344@gmail.com>
2022-07-03 20:03:42 +0300

    Finished the game board.

--- src/Common/GameBoard.js
+++ src/Common/GameBoard.js
@@ -1,9 +1,28 @@
-export class GameBoard {
+class GameBoard {
   #_board;
-  constructor(boardValues) {
+  #_boardLength;
+  constructor(boardValues, boardLength) {
     this.#_board = boardValues;
+    this.#_boardLength = boardLength;
   }
+
   get Data() {
     return this.#_board;
+  }
+  get BoardLength() {
+    return this.#_boardLength;
+  }
+  ElementAt(x, y) {
+    return this.#_board[this.#CalculatePosition(x, y)];
+  }
+  SwapPosition(sourceX, sourceY, destX, destY) {
+    const sourcePos = this.#CalculatePosition(sourceX, sourceY);
+    const destPos = this.#CalculatePosition(destX, destY);
+    let tempValue = this.#_board[sourcePos];
+    this.#_board[sourcePos] = this.#_board[destPos];
+    this.#_board[destPos] = tempValue;
+  }
+  #CalculatePosition(x, y) {
+    return x + y * this.#_boardLength;
   }
 }
