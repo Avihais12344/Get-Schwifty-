12c75df5dd12c576238ce4aa21077cdeaae32a31
Avihai Shalom <avihais12344@gmail.com>
2022-07-03 20:05:09 +0300

    Added tests for the game board (not the best tests, but what I could have done.

---
+++ test/common/GameBoardTest.js
@@ -0,0 +1,12 @@
+// Testing.
+function GameBoard_WhenSwappingItemsOnGameBoard_InsureSwappingIsCorrect() {
+  // Arrange.
+  let board = new GameBoard([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+  let valueAt00 = board.ElementAt(0, 0);
+  let valueAt11 = board.ElementAt(1, 1);
+  // Act. (swap beteen 1 and 5).
+  board.SwapPosition(0, 0, 1, 1);
+  return (
+    valueAt00 == board.ElementAt(1, 1) && valueAt11 == board.ElementAt(0, 0)
+  );
+}
