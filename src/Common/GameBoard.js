37d777db81447285c5a533267c907d55b05faa00
Avihai Shalom <avihais12344@gmail.com>
2022-07-03 20:57:54 +0300

    Added a function to scan the differences between 2 boards.

--- src/Common/GameBoard.js
+++ src/Common/GameBoard.js
@@ -22,6 +22,22 @@ class GameBoard {
     this.#_board[sourcePos] = this.#_board[destPos];
     this.#_board[destPos] = tempValue;
   }
+  ScanDifferences(otherBoard) {
+    if (this.BoardLength != otherBoard.BoardLength) {
+      return undefined;
+    }
+    let diffs = [];
+    //run on y.
+    for (y = 0; y < this.#_boardLength; y++) {
+      // run on x.
+      for (x = 0; x < this.#_boardLength; x++) {
+        if (this.ElementAt(x, y) != otherBoard.ElementAt(x, y)) {
+          diffs.push({ X: x, Y: y });
+        }
+      }
+    }
+    return diffs;
+  }
   #CalculatePosition(x, y) {
     return x + y * this.#_boardLength;
   }
