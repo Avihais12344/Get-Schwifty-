358ee6c4f93b4987ac9cc06ec6823e93bea74952
Avihai Shalom <avihais12344@gmail.com>
2022-07-04 19:54:00 +0300

    Added the application itself.

--- Src/app.js
+++ Src/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+class App {
+  Game;
+  BoardDisplay;
+  constructor(boardElement) {
+    this.#GenerateGame();
+    // Generate the display.
+    this.BoardDisplay = new HTMLBoardDisplay(
+      this.Game.GameBoard,
+      this.Game.OnNewInput,
+      boardElement
+    );
+  }
+  #GenerateGame() {
+    let rng = new RNGBase();
+    let boardGenerator = new NumbersBoardGenerator(rng);
+    let boardChecker = new NumbersBoardChecker();
+    let inputChecker = new InputChecker();
+    let winningChecker = new WinningChecker();
+    this.Game = new Game(
+      inputChecker,
+      boardGenerator,
+      boardChecker,
+      winningChecker
+    );
+  }
+}
