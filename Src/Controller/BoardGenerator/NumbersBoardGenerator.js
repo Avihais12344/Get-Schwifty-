47a19f645d7cf9ccac2b1b169a2821cfb44572f9
Avihai Shalom <avihais12344@gmail.com>
2022-07-04 12:48:37 +0300

    Fixed bugs with the numbers board generator.

---
+++ Src/Controller/BoardGenerator/NumbersBoardGenerator.js
@@ -0,0 +1,19 @@
+class NumbersBoardGenerator extends BoardGeneratorBase {
+  #_rng;
+  constructor(rng) {
+    super();
+    this.#_rng = rng;
+  }
+  GenerateBoard(boardParams) {
+    const boardLen = boardParams.Length;
+    const maxValue = boardLen * boardLen;
+    console.debug("Board length " + boardLen + " max value " + maxValue);
+    let numbersGenerated = new Set();
+    while (numbersGenerated.size < maxValue) {
+      numbersGenerated.add(this.#_rng.GenerateNumber(0, maxValue - 1));
+    }
+    console.log(numbersGenerated);
+    console.log("generated board: " + Array.from(numbersGenerated));
+    return new GameBoard(Array.from(numbersGenerated));
+  }
+}
