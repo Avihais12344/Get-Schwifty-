04448ce19528708a4ad0f9669de3e9b2a5533d94
Avihai Shalom <avihais12344@gmail.com>
2022-07-04 16:58:29 +0300

    Fixed the check winning function after doing some testing.

--- Src/Controller/WinChecker/WinningChecker.js
+++ Src/Controller/WinChecker/WinningChecker.js
@@ -3,12 +3,20 @@ class WinningChecker {
     let currExpectedValue = boardToCheck.EmptyCellValue + 1;
     const maxValue = boardToCheck.BoardLength * boardToCheck.BoardLength - 1;
     const boardCells = boardToCheck.Data;
-    for (let item in boardCells) {
+    console.debug("maxValue = " + maxValue);
+    console.debug("boardCells = " + boardCells);
+    for (let index = 0; index < maxValue + 1; index++) {
+      console.debug("item = " + boardCells[index]);
+      console.debug("currExpectedValue = " + currExpectedValue);
       // If it's not the item we are expecting
       // and we are not at the empty cell.
-      if (item != currExpectedValue && currExpectedValue <= maxValue) {
+      if (
+        boardCells[index] != currExpectedValue &&
+        currExpectedValue <= maxValue
+      ) {
         return false;
       }
+      currExpectedValue++;
     }
     return true;
   }
