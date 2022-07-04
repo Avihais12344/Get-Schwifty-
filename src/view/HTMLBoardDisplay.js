6b01d3eda40a8407549b38bd03825169b7f28302
Avihai Shalom <avihais12344@gmail.com>
2022-07-04 10:36:49 +0300

    Added the preperation of the board and it's first display.

--- src/view/HTMLBoardDisplay.js
+++ src/view/HTMLBoardDisplay.js
@@ -1,10 +1,10 @@
 class HTMLBoardDisplay extends BoardDisplayBase {
-  #_cellsIds;
   #_oldBoard;
-  constructor(cellsIds, board) {
-    this.#_cellsIds = cellsIds;
-    this.#_oldBoard = board;
+  constructor(board, callbackFunctionOnClick, boardElement) {
     super();
+    this.#_oldBoard = board;
+    // Create the board.
+    this.#PrepareBoard(callbackFunctionOnClick, boardElement);
   }
   UpdateDisplay(newBoard) {
     //Detect where the change has happened.
@@ -16,12 +16,34 @@ class HTMLBoardDisplay extends BoardDisplayBase {
     // Update the board.
     this.#_oldBoard = newBoard;
   }
-  get BoardCellsId() {
-    return this.#_cellsIds;
-  }
   // todo.
   #SwapCells(oldPosition, newPosition) {}
   #DifferenceToCellId(change) {
     return change.X.toString() + "," + change.Y.toString();
+  }
+  #PrepareBoard(callbackFunctionOnClick, boardElement) {
+    const boardLength = this.#_oldBoard.BoardLength;
+    for (let y = 0; y < boardLength; y++) {
+      for (let x = 0; x < boardLength; x++) {
+        const buttonElement = document.createElement("button");
+        const elementAtPos = this.#_oldBoard.ElementAt(x, y);
+        // Add the event of clicking.
+        buttonElement.addEventListener("click", () => {
+          console.debug("User clicked on button: (" + x + ", " + y + ")");
+          callbackFunctionOnClick(x, y);
+        });
+        // the text in the button.
+        buttonElement.innerText = this.#GenerateTextByValue(elementAtPos);
+        // the id of the button.
+        buttonElement.setAttribute(
+          "id",
+          this.#DifferenceToCellId({ X: x, Y: y })
+        );
+        boardElement.appendChild(buttonElement);
+      }
+    }
+  }
+  #GenerateTextByValue(value) {
+    return value ? value : "";
   }
 }
